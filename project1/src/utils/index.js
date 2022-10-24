00fa70b0d607571f4d1ebbec3fcd52f0261f66d8
adolfvitor <tong.shen@17dianjia.com>
2022-10-24 19:47:49 +0800

    max-file-upload

--- project1/src/utils/index.js
+++ project1/src/utils/index.js
@@ -1,6 +1,29 @@
 import  Methords  from './methords'
 import * as Validate from './validate'
 
+export function request({
+	url,
+	method = 'post',
+	data,
+	headers = {},
+	requestList
+	}) {
+		return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest()
+			xhr.open(method, url)
+			Object.keys(headers).forEach(key =>
+				xhr.setRequestHeader(key, headers[key])
+			)
+			xhr.send(data)
+			xhr.onload = e => {
+				resolve({
+					data: e.target.response
+				})
+			}
+			// xhr.onerror = (err) => reject(err)
+		})
+	}
+
 export default Object.assign(
 	{},
 	Methords,
--- project1/src/views/upload/index.vue
+++ project1/src/views/upload/index.vue
@@ -6,12 +6,16 @@
 </template>
 
 <script>
+	const SIZE = 10 * 1024 * 10
+
+	import { request } from '@/utils'
 	export default {
 	data () {
 		return {
 			container: {
 				file: null
-			}
+			},
+			data: []
 		}
 	},
 	mounted() {
@@ -25,7 +29,56 @@
 			Object.assign(this.$data, this.$options.data())
 			this.container.file = file
 		},
-		async handleUpload() {}
+		// 生成切片文件
+		createFileChunk(file, size = SIZE) {
+			const fileChunkList = []
+			let cur = 0
+			while (cur < file.size) {
+				fileChunkList.push({ file: file.slice(cur, cur + size) })
+				cur += size
+			}
+			return fileChunkList
+		},
+		async handleUpload() {
+			if (!this.container.file) return
+			const fileChunkList = this.createFileChunk(this.container.file)
+			this.data = fileChunkList.map(({ file }, index) => ({
+				chunk: file,
+				// 文件名 + 数组下标
+				hash: this.container.file.name + '-' + index
+			}))
+			await this.uploadChunk()
+		},
+		// 上传切片
+		async uploadChunk() {
+			const requstList = this.data.map(({ chunk, hash }) => {
+				const formData = new FormData()
+				formData.append('chunk', chunk)
+				formData.append('hash', hash)
+				formData.append('filename', this.container.file.name)
+				return { formData }
+			}).map(({ formData }) =>
+				(request({ url: 'http://localhost:3000', data: formData }))
+			)
+
+			console.log('requstList', requstList)
+
+			// 并发请求
+			await Promise.all(requstList)
+			// 合并切片
+			await this.mergeRequst()
+		},
+		async mergeRequst() {
+			await this.request({
+				url: 'http://localhost:3000/merge',
+				headers: {
+					'content-type': 'application/json'
+				},
+				data: JSON.stringify({
+					filename: this.container.file.name
+				})
+			})
+		}
 	}
 }
 </script>
